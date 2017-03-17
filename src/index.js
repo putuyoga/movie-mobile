a40565cbec37317ffc91e5bba3a01f33ec8fe46f
I Putu Yoga Permana <putuyoga@gmail.com>
2017-03-17 11:53:51 +0700

    Added redux-remote-devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import {
   Text,
 } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import AppWithNavigationState from './navigations/appWithNavigationState';
 import appReducers from './reducers';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
-const store = createStore(appReducers);
+const store = createStore(appReducers, composeWithDevTools(applyMiddleware(thunk)));
+
+import { loadMovies } from './actions/movie';
+store.dispatch(loadMovies());
 
 class Main extends React.Component {
   render() {
